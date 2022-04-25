89f4f8a9c8e6a72f06445603c1a163afcc6012d6
Erin Sparling <erin@everyplace.net>
2022-04-25 05:55:27 +0100

    Bumping version

--- startup/ip.py
+++ startup/ip.py
@@ -19,7 +19,7 @@ with open('package.json') as f:
     VERSION = d['version']
 """
 # For some reason, the above code works after boot, but not when launched by a cron?
-VERSION="0.1.1"
+VERSION="0.1.2"
 
 import ST7789
 import socket
