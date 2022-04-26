e65511695e19b27503e7d92a7e5f021df7aa8c7e
Erin Sparling <erin@everyplace.net>
2022-04-26 05:35:14 +0000

    Added some space

--- startup/public/components/remote.js
+++ startup/public/components/remote.js
@@ -8,7 +8,7 @@ const remote = document.createElement('a')
 remote.setAttribute('href','/edit')
 remote.innerText = "Edit your project"
 remote.classList.add('mdl-button','mdl-js-button','mdl-button--raised','mdl-js-ripple-effect','mdl-button--accent')
-
+remote.setAttribute('style','margin-top:15px;')
 
 const url = await fetch('/remote')
   .then(r=>r.json())
