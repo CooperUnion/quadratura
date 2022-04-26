4a15713f7e49477b66dee04622026a12e36c56a4
Erin Sparling <erin@everyplace.net>
2022-04-26 04:28:29 +0000

    window.onload === no

--- startup/public/script.js
+++ startup/public/script.js
@@ -3,13 +3,9 @@ import services from './components/services.js'
 import snackbar from './components/snackbar.js'
 import wifi from './components/wifi.js'
 
-window.onload = async ()=>{
-
-  const body = document.querySelector('body')
-  body.appendChild(title)
-  body.appendChild(services)
-  body.appendChild(wifi)
-  body.appendChild(snackbar)
-  componentHandler.upgradeDom()
-}
-
+const body = document.querySelector('body')
+body.appendChild(title)
+body.appendChild(services)
+body.appendChild(wifi)
+body.appendChild(snackbar)
+componentHandler.upgradeDom()
