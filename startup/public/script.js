184fe4b50a27dc7d422ec88810163fd2648c4569
Erin Sparling <erin@everyplace.net>
2022-04-26 05:07:07 +0000

    Adding the form to the main page

--- startup/public/script.js
+++ startup/public/script.js
@@ -1,5 +1,6 @@
 import title from './components/title.js'
 import services from './components/services.js'
+import remote from './components/remote.js'
 import snackbar from './components/snackbar.js'
 import wifi from './components/wifi.js'
 
