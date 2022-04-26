bb491c8dbcc08e87c23cf5ef1f737676598766cc
Erin Sparling <erin@everyplace.net>
2022-04-26 02:46:43 +0100

    Disabling codeserver enabled

--- startup/update.js
+++ startup/update.js
@@ -2,8 +2,10 @@
 import { exec, execSync } from 'child_process'
 import updatePort from '../update/updatePort.mjs'
 import updateForSpecificHost from '../update/updateForSpecificHost.mjs'
+import disableCodeserver from '../update/disableCodeserver.mjs'
 
 export function update() {
   updatePort()
   updateForSpecificHost()
+  disableCodeserver()
 }
