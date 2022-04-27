bb531bb4c1d071c8bf70f469edf3772e0a2b128d
Erin Sparling <erin@everyplace.net>
2022-04-27 02:54:05 +0100

    Enabled service rewriting.

--- startup/update.js
+++ startup/update.js
@@ -3,9 +3,11 @@ import { exec, execSync } from 'child_process'
 import updatePort from '../update/updatePort.mjs'
 import updateForSpecificHost from '../update/updateForSpecificHost.mjs'
 import disableCodeserver from '../update/disableCodeserver.mjs'
+import rewriteServices from '../update/rewriteIPMenuAndDisplay.mjs'
 
 export function update() {
   updatePort()
   updateForSpecificHost()
   disableCodeserver()
+  rewriteServices()
 }
